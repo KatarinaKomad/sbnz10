8656deea9a6828d793845ce670ccf3b62b3275ef
nl16 <natasha.lakovic@gmail.com>
2023-05-17 17:38:15 +0200

    Initializacija backward pravila

--- backend/model/src/main/resources/data-postgres.sql
+++ backend/model/src/main/resources/data-postgres.sql
@@ -1,3 +1,39 @@
 insert into KORISNIK(email, ime, prezime, kategorija, poeni) VALUES
 ('komad.katarina@gmail.com', 'Katarina', 'Komad', 'REGULAR', 0),
 ('natasha.lakovic@gmail.com', 'Natasa', 'Lakovic', 'REGULAR', 0);
+
+insert into SIMPTOM(lokacija, opis) VALUES
+('LIST', 'svetlo-smeđe pege'), 
+('LIST', 'maslonasto zelene pege'),
+('LIST', 'uvijanje'),
+('LIST', 'opadanje'),
+('LIST', 'sušenje'),
+('LIST', 'braon larve'),
+('LIST', 'bele tačkice'),
+('LIST', 'žuta boja'),
+('LIST', 'crveni pauci'),
+('LIST', 'paukova mreža (naličje)'),
+('LIST', 'mravi'),
+('LIST', 'leprljivost'),
+('LIST', 'crne larve'),
+('LIST', 'mrke fleke sa zelenim okvirom'),
+('LIST', 'bele linije'),
+('LIST', 'zadebljanje'),
+('LIST', 'crveno-ljubičasta boja'),
+('PLOD', 'crvene fleke'),
+('PLOD', 'opadanje zelenih plodova'),
+('PLOD', 'gusenice'),
+('PLOD', 'trulež'),
+('PLOD', 'izmet insekta'),
+('PLOD', 'braon fleke'),
+('PLOD', 'plesan'),
+('PLOD', 'larve'),
+('CVET', 'tamno siva boja'),
+('CVET', 'opadanje'),
+('CVET', 'sušenje'),
+('CVET', 'zelene tačke'),
+('GRANA', 'braon larve'),
+('GRANA', 'suve');
+
+-- INSERT INTO PREPARAT (naziv, primarnaKategorija, potkategorija, ocene) VALUES
+
